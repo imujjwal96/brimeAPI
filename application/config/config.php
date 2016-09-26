e5aea68f6d1455f88afe40a6bbc9bd378e8dc85a
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-09-26 17:10:50 +0530

    Managed Configurations

--- application/config/config.php
+++ application/config/config.php
@@ -5,6 +5,13 @@ ini_set("display_errors", 1);
 
 return array(
     'URL' => 'http://' . $_SERVER['HTTP_HOST'] . str_replace('public', '', dirname($_SERVER['SCRIPT_NAME'])),
+
+    'PATH_CONTROLLER' => realpath(dirname(__FILE__).'/../../') . '/application/controllers/',
+    'PATH_VIEW' => realpath(dirname(__FILE__).'/../../') . '/application/views/',
+
+    'DEFAULT_CONTROLLER' => 'index',
+    'DEFAULT_ACTION' => 'index',
+
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'brime_main',
--- application/core/Application.php
+++ application/core/Application.php
@@ -2,12 +2,44 @@
 
 class Application {
 
-    protected $controller = 'IndexController';
-    protected $method = 'index';
+    protected $controller;
+    protected $method;
     protected $params = array();
+    private $controllerName;
 
     public function __construct() {
         $url = $this->parseURL();
+
+        if (!$this->controllerName) {
+            $this->controllerName = Config::get('DEFAULT_CONTROLLER');
+        }
+
+        if (!$this->method OR (strlen($this->method) == 0)) {
+            $this->method = Config::get('DEFAULT_ACTION');
+        }
+
+        $this->controllerName = ucwords($this->controllerName) . 'Controller';
+
+        if (file_exists(Config::get('PATH_CONTROLLER') . $this->controllerName . '.php')) {
+
+            require Config::get('PATH_CONTROLLER') . $this->controllerName . '.php';
+            $this->controller = new $this->controllerName();
+
+            if (method_exists($this->controller, $this->method)) {
+                if (!empty($this->params)) {
+                    call_user_func_array(array($this->controller, $this->method), $this->params);
+                } else {
+                    $this->controller->{$this->method}();
+                }
+            } else {
+                // Error 404
+            }
+        } else {
+            // Error 404
+        }
+
+
+        /*
 
         if (file_exists('../application/controllers/' . $url[0] . 'Controller.php')) {
             $this->controller = ucwords($url[0]) . 'Controller';
@@ -26,12 +58,21 @@ class Application {
 
         $this->params = $url ? array_values($url) : [];
 
-        call_user_func_array([$this->controller, $this->method], $this->params);
+        call_user_func_array([$this->controller, $this->method], $this->params);*/
     }
 
     public function parseURL() {
         if (isset($_GET['url'])) {
-            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+            $url = trim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            $this->controllerName = isset($url[0]) ? $url[0] : null;
+            $this->method = isset($url[1]) ? $url[1] : null;
+
+            unset($url[0], $url[1]);
+
+            $this->params = array_values($url);
         }
     }
 }
