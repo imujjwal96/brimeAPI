5012fa0bd2e9b03b05d6eb2e6f89be67f380f8ec
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-12-30 00:44:07 +0530

    Add Eloquent

--- Application/Controllers/HomeController.php
+++ Application/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace Brime\Controllers;
 
+use Brime\Models\User;
 use Slim\Views\Twig as View;
 
 class HomeController extends Controller
 {
     public function index($request, $response)
     {
+        $user = User::find(61);
+        echo '<pre>';
+        var_dump($user->email);
+
+        die();
         return $this->view->render($response, 'home.twig');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,30 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'brime_main',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => ''
+        ]
     ]
 ]);
 
 $container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
+};
 
 $container["view"] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
